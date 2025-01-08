61fc83f3bd2d3ae2b6a50def4b1c00b603d57a98
Joao-Campos-Cunha <95706397+Joao-Campos-Cunha@users.noreply.github.com>
2025-01-08 15:51:49 +0000

    Delete app/src/main/java/com/example/lojasocial directory

--- app/src/main/java/com/example/lojasocial/domain/repository/StatisticsRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lojasocial.domain.repository
-
-interface StatisticsRepository {
-    data class BeneficiaryStats(val month: String, val count: Int)
-    data class NationalityStats(val country: String, val count: Int)
-    data class ItemStats(val itemName: String, val percentage: Float)
-
-}
--- app/src/main/java/com/example/lojasocial/presentation/statistics/components/NationalityStats.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lojasocial.presentation.statistics.components
-
-data class NationalityStats(
-    val country: String,
-    val count: Int
-)

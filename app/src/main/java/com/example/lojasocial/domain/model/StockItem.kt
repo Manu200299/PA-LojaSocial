1ec86fda1499cfb17c080fe5d86ddacd5d65cef2
Guilherme Azeredo <95869724+azeredo-99@users.noreply.github.com>
2025-01-08 18:46:39 +0000

    Delete app directory

--- app/src/main/java/com/example/lojasocial/domain/model/StockItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lojasocial.domain.model
-
-data class StockItem(
-    val itemId: String,
-    val nome: String,
-    val quantidade: Int,
-    val categoria: String,
-    val isEssential: Boolean
-)
